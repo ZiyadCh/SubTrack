26f3d735b5520fe36b2568cc3d540d994e82b89f
ziyad <chetoziyad@gmail.com>
2026-09-16 16:40:11 +0100

    added a loop on main

--- Main.java
+++ Main.java
@@ -14,27 +14,31 @@ public class Main {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.println("┌────────────────────────────────┐");
-    System.out.println("│      Sélectionnez une action   │");
-    System.out.println("├────────────────────────────────┤");
-    System.out.println("│1❯ Lister les Abonnements       │");
-    System.out.println("│2❯ Ajouter un Abonnement        │");
-    System.out.println("│3❯ Modifier un Abonnement       │");
-    System.out.println("│4❯ Supprimer un Abonnement      │");
-    System.out.println("│5❯ Paiements                    │");
-    System.out.println("│6❯ Ajouter un Paiement          │");
-    System.out.println("│7❯ Quitter                      │");
-    System.out.println("└────────────────────────────────┘");
-    switch (scanner.nextLine()) {
-      case "1":
-        abonnementService.listAbonnement();
-        break;
 
-      case "2":
-        return;
+    while (true) {
+      System.out.println("┌────────────────────────────────┐");
+      System.out.println("│      Sélectionnez une action   │");
+      System.out.println("├────────────────────────────────┤");
+      System.out.println("│1❯ Lister les Abonnements       │");
+      System.out.println("│2❯ Ajouter un Abonnement        │");
+      System.out.println("│3❯ Modifier un Abonnement       │");
+      System.out.println("│4❯ Supprimer un Abonnement      │");
+      System.out.println("│5❯ Paiements                    │");
+      System.out.println("│6❯ Ajouter un Paiement          │");
+      System.out.println("│7❯ Quitter                      │");
+      System.out.println("└────────────────────────────────┘");
+      switch (scanner.nextLine()) {
+        case "1":
+          abonnementService.listAbonnement();
+          break;
 
-      default:
-        break;
+        case "7":
+          System.out.println("Au revoir !");
+          return;
+
+        default:
+          break;
+      }
     }
   }
 
